ddfbb5fce34ba41012f81739db34a754ee0a61c8
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-10-09 12:35:16 +0530

    feat: home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Plus, PieChart, Calculator } from "lucide-react";
+import { Plus, PieChart } from "lucide-react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
@@ -9,10 +10,14 @@ export default function Home() {
           <div>
             <h1 className="text-xl font-bold">Project Name</h1>
           </div>
+          <div>
+            <Avatar>
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
         </div>
       </nav>
 
-      {/* Main Content */}
       <main className="flex-1 p-8">
         <header className="flex justify-between items-center mb-8">
           <div>
@@ -23,15 +28,12 @@ export default function Home() {
           </div>
         </header>
 
-        <div className="grid grid-cols-2 gap-6">
-          {/* Repeated Cards */}
-          {[...Array(4)].map((_, index) => (
-            <div key={index} className="bg-white p-6 rounded-lg shadow-sm">
+        <div className="grid grid-cols-3 gap-6">
+          {[...Array(6)].map((_, index) => (
+            <div key={index} className="bg-white   p-6 rounded-lg shadow-sm">
               <div className="flex justify-between items-center mb-4">
+                <h3 className="underline"> # {index + 1}</h3>
                 <h3 className="text-xl font-semibold">Maths CT - 3</h3>
-                <span className="p-2 bg-blue-100 rounded-full">
-                  <Calculator className="text-blue-600" />
-                </span>
               </div>
               <div className="space-y-2 mb-4">
                 <div className="flex justify-between">
